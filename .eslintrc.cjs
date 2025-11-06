da46f4dfcbe8f6bce1856ebd101203b95ab0e9bd
Batohack <dorianbat8@gmail.com>
2025-11-06 12:29:32 +0100

    feat: configure ESLint with TypeScript and React rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+
+module.exports = {
+  
+  root: true,
+  
+  env: { browser: true, es2020: true, node: true },
+  
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  
+  
+  parser: '@typescript-eslint/parser',
+  
+  
+  parserOptions: { 
+    ecmaVersion: 'latest', 
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+    project: './tsconfig.json' 
+
+  },
+  
+  
+  plugins: ['react-refresh'],
+  
+  
+  rules: {
+    
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+
+    
+  },
+  
+  
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+
+};
