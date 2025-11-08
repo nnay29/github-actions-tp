4a041f1e63d5cfe1420e35f02b3383023a38ade4
nnay29 <fodoptallayann@gmail.com>
2025-11-08 06:35:12 +0100

    test intentionnel du CI pour verifier validation des conventions

---
+++ src/components/TestComposant.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function TestComposant() {
+    const unusedvar1;
+
+    const name:string = 153;
+    
+  return (
+    <div>TestComposant doit echouer car ne se termine par 'Love'1</div>
+  )
+}
