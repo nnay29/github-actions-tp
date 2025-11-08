86768268b26ffba4452a74912e790d4380ee2af8
nnay29 <fodoptallayann@gmail.com>
2025-11-08 07:58:32 +0100

    test composant 7h58

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default function TestComponent() {
+    
+    const unusedBad;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const unusedOkay;
+
+    const nom:string = 14;
+
+  return (
+    <div>TestComponent</div>
+  )
+}
